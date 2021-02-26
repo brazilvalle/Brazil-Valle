4c08ebdd453114ad0bd557362bbf326b4849d021
feernando <fernandofitilan@hotmail.com>
2021-02-26 19:32:00 -0300

    subindo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent} from './home/home.component'
+import { PageaboutComponent} from './pageabout/pageabout.component'
+import { ServicesComponent} from './services/services.component'
+import { ContatusComponent} from './contatus/contatus.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }, 
+  { path: 'sobre', component: PageaboutComponent },
+  { path: 'servico', component: ServicesComponent },   
+  { path: 'contato', component: ContatusComponent },   
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
